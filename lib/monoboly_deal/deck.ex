f5befcb3021797554109ee09a50f2f3ea40b9092
Marc Delagrammatikas <delagrammatikas@gmail.com>
2018-11-24 22:38:21 -0800

    Adds Deck module which contains a list of all possible cards

---
+++ lib/monoboly_deal/deck.ex
@@ -0,0 +1,132 @@
+defmodule MonobolyDeal.Deck do
+  alias MonobolyDeal.Deck.{
+    MoneyCard,
+    ActionCard,
+    RentCard,
+    PropertyCard,
+    DualPropertyCard,
+    PropertyWildCard
+  }
+
+  def cards do
+    []
+    |> Kernel.++(for _ <- 1..6, do: %MoneyCard{value: 1})
+    |> Kernel.++(for _ <- 1..5, do: %MoneyCard{value: 2})
+    |> Kernel.++(for _ <- 1..3, do: %MoneyCard{value: 3})
+    |> Kernel.++(for _ <- 1..3, do: %MoneyCard{value: 4})
+    |> Kernel.++(for _ <- 1..2, do: %MoneyCard{value: 5})
+    |> Kernel.++([%MoneyCard{value: 10}])
+    |> Kernel.++(for _ <- 1..2, do: %ActionCard{value: 5, type: :deal_breaker})
+    |> Kernel.++(for _ <- 1..3, do: %ActionCard{value: 4, type: :just_say_no})
+    |> Kernel.++(for _ <- 1..10, do: %ActionCard{value: 1, type: :pass_go})
+    |> Kernel.++(for _ <- 1..3, do: %ActionCard{value: 3, type: :forced_deal})
+    |> Kernel.++(for _ <- 1..3, do: %ActionCard{value: 3, type: :sly_deal})
+    |> Kernel.++(for _ <- 1..3, do: %ActionCard{value: 3, type: :debt_collectors})
+    |> Kernel.++(for _ <- 1..3, do: %ActionCard{value: 2, type: :its_my_birthday})
+    |> Kernel.++(for _ <- 1..2, do: %ActionCard{value: 1, type: :double_the_rent})
+    |> Kernel.++(for _ <- 1..3, do: %ActionCard{value: 3, type: :house})
+    |> Kernel.++(for _ <- 1..2, do: %ActionCard{value: 4, type: :hotel})
+    |> Kernel.++(for _ <- 1..2, do: %RentCard{value: 1, colors: [:blue, :green]})
+    |> Kernel.++(for _ <- 1..2, do: %RentCard{value: 1, colors: [:red, :yellow]})
+    |> Kernel.++(for _ <- 1..2, do: %RentCard{value: 1, colors: [:pink, :orange]})
+    |> Kernel.++(for _ <- 1..2, do: %RentCard{value: 1, colors: [:light_blue, :brown]})
+    |> Kernel.++(for _ <- 1..2, do: %RentCard{value: 1, colors: [:railroad, :utility]})
+    |> Kernel.++(
+      for _ <- 1..3,
+          do: %RentCard{
+            value: 3,
+            colors: [
+              :blue,
+              :green,
+              :red,
+              :yellow,
+              :pink,
+              :orange,
+              :light_blue,
+              :brown,
+              :railroad,
+              :utility
+            ]
+          }
+    )
+    |> Kernel.++(for _ <- 1..2, do: %PropertyCard{value: 4, color: :blue, rent_values: [3, 8]})
+    |> Kernel.++(for _ <- 1..2, do: %PropertyCard{value: 1, color: :brown, rent_values: [1, 2]})
+    |> Kernel.++(for _ <- 1..2, do: %PropertyCard{value: 2, color: :utility, rent_values: [1, 2]})
+    |> Kernel.++(
+      for _ <- 1..3, do: %PropertyCard{value: 4, color: :green, rent_values: [2, 4, 7]}
+    )
+    |> Kernel.++(
+      for _ <- 1..3, do: %PropertyCard{value: 3, color: :yellow, rent_values: [2, 4, 6]}
+    )
+    |> Kernel.++(for _ <- 1..3, do: %PropertyCard{value: 3, color: :red, rent_values: [2, 3, 6]})
+    |> Kernel.++(
+      for _ <- 1..3, do: %PropertyCard{value: 2, color: :orange, rent_values: [1, 3, 5]}
+    )
+    |> Kernel.++(for _ <- 1..3, do: %PropertyCard{value: 2, color: :pink, rent_values: [1, 2, 4]})
+    |> Kernel.++(
+      for _ <- 1..3, do: %PropertyCard{value: 1, color: :light_blue, rent_values: [1, 2, 3]}
+    )
+    |> Kernel.++(
+      for _ <- 1..4, do: %PropertyCard{value: 2, color: :railroad, rent_values: [1, 2, 3, 4]}
+    )
+    |> Kernel.++([
+      %DualPropertyCard{
+        properties: [
+          %PropertyCard{value: 4, color: :blue, rent_values: [3, 8]},
+          %PropertyCard{value: 4, color: :green, rent_values: [2, 4, 7]}
+        ]
+      }
+    ])
+    |> Kernel.++([
+      %DualPropertyCard{
+        properties: [
+          %PropertyCard{value: 4, color: :green, rent_values: [2, 4, 7]},
+          %PropertyCard{value: 2, color: :railroad, rent_values: [1, 2, 3, 4]}
+        ]
+      }
+    ])
+    |> Kernel.++([
+      %DualPropertyCard{
+        properties: [
+          %PropertyCard{value: 2, color: :utility, rent_values: [1, 2]},
+          %PropertyCard{value: 2, color: :railroad, rent_values: [1, 2, 3, 4]}
+        ]
+      }
+    ])
+    |> Kernel.++([
+      %DualPropertyCard{
+        properties: [
+          %PropertyCard{value: 1, color: :light_blue, rent_values: [1, 2, 3]},
+          %PropertyCard{value: 2, color: :railroad, rent_values: [1, 2, 3, 4]}
+        ]
+      }
+    ])
+    |> Kernel.++([
+      %DualPropertyCard{
+        properties: [
+          %PropertyCard{value: 1, color: :light_blue, rent_values: [1, 2, 3]},
+          %PropertyCard{value: 1, color: :brown, rent_values: [1, 2]}
+        ]
+      }
+    ])
+    |> Kernel.++(
+      for _ <- 1..2,
+          do: %DualPropertyCard{
+            properties: [
+              %PropertyCard{value: 2, color: :pink, rent_values: [1, 2, 4]},
+              %PropertyCard{value: 2, color: :orange, rent_values: [1, 3, 5]}
+            ]
+          }
+    )
+    |> Kernel.++(
+      for _ <- 1..2,
+          do: %DualPropertyCard{
+            properties: [
+              %PropertyCard{value: 3, color: :red, rent_values: [2, 3, 6]},
+              %PropertyCard{value: 3, color: :yellow, rent_values: [2, 4, 6]}
+            ]
+          }
+    )
+    |> Kernel.++(for _ <- 1..2, do: %PropertyWildCard{})
+  end
+end
---
+++ test/monoboly_deal/deck_test.exs
@@ -0,0 +1,51 @@
+defmodule MonobolyDeal.DeckTest do
+  use ExUnit.Case
+
+  alias MonobolyDeal.Deck
+
+  alias MonobolyDeal.Deck.{
+    MoneyCard,
+    ActionCard,
+    RentCard,
+    PropertyCard,
+    DualPropertyCard,
+    PropertyWildCard
+  }
+
+  describe "cards/0" do
+    test "returns 106 cards" do
+      count = Deck.cards() |> Enum.count()
+      assert count == 106
+    end
+
+    test "returns 20 money cards" do
+      cards = for %MoneyCard{} = card <- Deck.cards(), do: card
+      assert Enum.count(cards) == 20
+    end
+
+    test "returns 34 action cards" do
+      cards = for %ActionCard{} = card <- Deck.cards(), do: card
+      assert Enum.count(cards) == 34
+    end
+
+    test "returns 13 rent cards" do
+      cards = for %RentCard{} = card <- Deck.cards(), do: card
+      assert Enum.count(cards) == 13
+    end
+
+    test "returns 28 property cards" do
+      cards = for %PropertyCard{} = card <- Deck.cards(), do: card
+      assert Enum.count(cards) == 28
+    end
+
+    test "returns 9 dual property cards" do
+      cards = for %DualPropertyCard{} = card <- Deck.cards(), do: card
+      assert Enum.count(cards) == 9
+    end
+
+    test "returns 2 property wild cards" do
+      cards = for %PropertyWildCard{} = card <- Deck.cards(), do: card
+      assert Enum.count(cards) == 2
+    end
+  end
+end
