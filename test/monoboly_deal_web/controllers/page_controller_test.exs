6403b64c0be0a8413cf3274f852e0d7a30bc9d5e
Marc Delagrammatikas <delagrammatikas@gmail.com>
2018-11-25 12:35:38 -0800

    Update layout and index page

--- test/monoboly_deal_web/controllers/page_controller_test.exs
+++ test/monoboly_deal_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule MonobolyDealWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Monoboly Deal!"
   end
 end
